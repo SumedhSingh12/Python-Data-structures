fdd570312b73bc4e448808aab9a020012f2f6c8f
Sumedh Kumar Singh <sumedhsingh12@gmail.com>
2019-02-22 17:04:08 +0000

    String permutation program

---
+++ Recursion/String_permutations.py
@@ -0,0 +1,10 @@
+def permute(s):
+    output = []
+    if len(s) <= 1:
+        output = [s]
+    else:
+        for index,letter in enumerate(s):
+            for perm in permute(s[:index] + s[index+1:]):
+                output += [letter+perm]
+    return output
+permute('abc')
