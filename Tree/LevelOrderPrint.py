aa9c980dc35ba4290d53612c5e4553ce87bda978
Sumedh Kumar Singh <sumedhsingh12@gmail.com>
2019-03-12 14:43:02 +0000

    Adding code for level order tree traversal

---
+++ Tree/LevelOrderPrint.py
@@ -0,0 +1,35 @@
+import collections
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+def level_order_print(node):
+    if node == None:
+        return
+    else:
+        current_count, next_count = 1,0
+        nodes = collections.deque([node])
+        while len(nodes) != 0:
+            currentNode = nodes.popleft()
+            current_count -= 1
+            print(currentNode.val, end =" ")
+            
+            if currentNode.left:
+                nodes.append(currentNode.left)
+                next_count += 1
+            if currentNode.right:
+                nodes.append(currentNode.right)
+                next_count += 1
+            if current_count == 0:
+                print("\n")
+                current_count, next_count = next_count, current_count
+
+root = Node(1)
+a = Node(2)
+b = Node(3)
+root.left = a
+root.right = b
+level_order_print(root)
