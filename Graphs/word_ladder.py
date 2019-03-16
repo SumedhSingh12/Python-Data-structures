8fcef7cd2b7f82c150378489a1c1983d5ec6b0d6
Sumedh Kumar Singh <sumedhsingh12@gmail.com>
2019-03-16 13:06:18 +0000

    Update: word_ladder.py

--- Graphs/word_ladder.py
+++ Graphs/word_ladder.py
@@ -1,3 +1,54 @@
+class Vertex:
+    
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}                   #{key=id, value=weight}
+        
+    def addNeighbour(self, neighbour, weight):
+        self.connectedTo[neighbour] = weight
+        
+    def getConnections(self):
+        return self.connectedTo.keys()
+    
+    def getWeight(self, n):
+        return self.connectedTo[n]
+    
+    def __str__(self):
+        return str(self.id) + "connected to " + str([x.id for x in self.connectedTo.keys()])
+    
+class Graph:
+    
+    def __init__(self):
+        self.verticesList = {}              #{key=Vertex(), value=id}
+        self.noOfVertices = 0
+        
+    def addVertex(self,key):
+        self.noOfVertices += 1
+        self.verticesList[key] = Vertex(key)
+        return self.verticesList[key]
+    
+    def getVertex(self, n):
+        if n in self.verticesList:
+            return self.verticesList[n]
+        else:
+            return None
+        
+    def addEdge(self, fromVertex, toVertex, weight=0):
+        if fromVertex not in self.verticesList:
+            self.addVertex(fromVertex)
+        if toVertex not in self.verticesList:
+            self.addVertex(toVertex)
+        self.verticesList[fromVertex].addNeighbour(self.verticesList[toVertex], weight)
+        
+    def getVertices(self):
+        return self.verticesList.keys()
+    
+    def __contains__(self, n):
+        return n in self.verticesList
+    
+    def __iter__(self):
+        return iter(self.verticesList.values())
+
 def word_ladder(word_list):
     g = Graph()
     d = {}
